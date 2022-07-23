90b85f7950ba965d0998bb876389f4a7bdf4dccb
Christopher Ho <christopherho1994@gmail.com>
2022-07-23 17:00:31 +0800

    Completed CH02

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,15 @@
 class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    @reply = "I don't care, get dressed and go to work!"
+
+    if @question.downcase == 'i am going to work!'
+      @reply = 'Great!'
+    elsif @question.include?('?')
+      @reply = 'Silly question, get dressed and go to work!'
+    end
+  end
 end
